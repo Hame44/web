efd68916646129f147dfad3f7fdf07e48fa1ecdf
Hame228 <dimabogatchuc@ukr.net>
2024-09-12 21:33:27 +0300

    lol

---
+++ pages/script.js
@@ -0,0 +1,19 @@
+let textNormal = true;
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('buttonn').addEventListener('click', myFunction);
+});
+
+function myFunction() {
+    if (textNormal) {
+        document.body.style.fontSize = "smaller";
+        textNormal=false;
+        document.getElementById("p").innerHTML = "larger";
+    }
+    else {
+        document.body.style.fontSize = "larger";
+        textNormal=true;
+        document.getElementById("p").innerHTML = "smaller";
+    }
+}
