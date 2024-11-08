e90517764597dac1b65683ed05ecb10b5d33df3f
Hame228 <dimabogatchuc@ukr.net>
2024-11-08 10:27:26 +0200

    add require

---
+++ pages/navigation.php
@@ -0,0 +1,16 @@
+<nav style="height: 100%;">
+            <ul class="list">
+                <div style="flex-grow: 1; display: flex; align-items: center; justify-content: center;"
+                    onclick="window.location.href='index.php'">
+                    <li>На головну</li>
+                </div>
+                <div style="flex-grow: 1; justify-content: center; display: flex; align-items: center;"
+                    onclick="window.location.href='table.php'">
+                    <li>Таблиця</li>
+                </div>
+                <div style="flex-grow: 1; justify-content: center; display: flex; align-items: center;"
+                    onclick="window.location.href='video.php'">
+                    <li>Відео</li>
+                </div>
+            </ul>
+        </nav>
--- pages/video.php
+++ pages/video.php
@@ -11,22 +11,7 @@
 
 <body>
     <header style="height: 8vh; border-bottom: 4px solid rgb(64, 64, 64); background-color: #193B3A;">
-        <nav style="height: 100%;">
-            <ul class="list">
-                <div style="flex-grow: 1; display: flex; align-items: center; justify-content: center;"
-                    onclick="window.location.href='index.php'">
-                    <li>На головну</li>
-                </div>
-                <div style="flex-grow: 1; justify-content: center; display: flex; align-items: center;"
-                    onclick="window.location.href='table.php'">
-                    <li>Таблиця</li>
-                </div>
-                <div style="flex-grow: 1; justify-content: center; display: flex; align-items: center;"
-                    onclick="window.location.href='video.php'">
-                    <li>Відео</li>
-                </div>
-            </ul>
-        </nav>
+    <?php require './navigation.php'; ?>
     </header>
     <div style="display: flex; height: calc(100vh - 18vh);">
 
